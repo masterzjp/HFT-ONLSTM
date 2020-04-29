b39c51e336a76fcc2d098ef2b4ab04656c9d649f
coder_zhao <1178516253@qq.com>
2020-04-30 00:06:30 +0800

    Update Transfer_DBpedia3.py

--- dbpedia/Transfer_DBpedia3.py
+++ dbpedia/Transfer_DBpedia3.py
@@ -13,12 +13,12 @@ max_features = 89098
 batch_size = 64
 embedding_dims = 300
 epochs = 100
-pretrained_w2v, _, _ = pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\data\emb_matrix_glove_300', 'rb'))
+pretrained_w2v, _, _ = pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\data\emb_matrix_glove_300', 'rb'))
 ################################################################
 print('Loading data...')
 
-x,y1,y2,y3,y1_pad,y2_pad,y3_pad = pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\data\DBP_txt_vector300dim_y1y2y3_10dim_zjp','rb'))
-pre_y2_pad = pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DB_pre_y2_id_pad_2','rb'))
+x,y1,y2,y3,y1_pad,y2_pad,y3_pad = pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\data\DBP_txt_vector300dim_y1y2y3_10dim_zjp','rb'))
+pre_y2_pad = pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DB_pre_y2_id_pad_2','rb'))
 ########################################################################################################################
 x_train,x_test,y3_train,y3_test=train_test_split( x, y3, test_size=0.2, random_state=42)
 #预测标签
@@ -32,12 +32,12 @@ emb_label_test = list(np.column_stack((pre_y2_test_pad,x_test)))
 #################################
 print('Build model...')
 model = TextONLSTM3(maxlen, max_features, embedding_dims, pretrained_w2v).get_model()
-model.load_weights(r"D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay1content_y2_best_weights.h5",by_name=True)
+model.load_weights(r"D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay1content_y2_best_weights.h5",by_name=True)
 #########################
 model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 print('Train...')
-fileweights = r"D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay2content_y3_best_weights.h5"
+fileweights = r"D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay2content_y3_best_weights.h5"
 checkpoint = ModelCheckpoint(fileweights, monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
 # checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
 early_stopping = EarlyStopping(monitor='val_acc', patience=5, mode='max')
