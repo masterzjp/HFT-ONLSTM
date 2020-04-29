773c502ee81743fcb28f2a84ea55753fce296056
coder_zhao <1178516253@qq.com>
2020-04-30 00:05:18 +0800

    Update Transfer_DBpedia2.py

--- dbpedia/Transfer_DBpedia2.py
+++ dbpedia/Transfer_DBpedia2.py
@@ -16,12 +16,12 @@ embedding_dims = 300
 epochs = 100
 
 start_time =time.time()
-pretrained_w2v, _, _ = pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\data\emb_matrix_glove_300', 'rb'))
+pretrained_w2v, _, _ = pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\data\emb_matrix_glove_300', 'rb'))
 ################################################################
 print('Loading data...')
 
-x,y1,y2,y3,y1_pad,y2_pad,y3_pad = pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\data\DBpedia\DBP_txt_vector300dim_y1y2y3_10dim_zjp','rb'))
-pre_y1_pad = pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DB_pre_y1_id_pad_2','rb'))
+x,y1,y2,y3,y1_pad,y2_pad,y3_pad = pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\data\DBpedia\DBP_txt_vector300dim_y1y2y3_10dim_zjp','rb'))
+pre_y1_pad = pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DB_pre_y1_id_pad_2','rb'))
 print(pre_y1_pad[:3])
 ######20191124 预测标签转为真实标签
 emb_pre_label_x = list(np.column_stack((pre_y1_pad,x)))
@@ -41,12 +41,12 @@ emb_label_test = list(np.column_stack((pre_y1_test_pad,x_test)))
 
 print('Build model...')
 model = TextONLSTM2(maxlen, max_features, embedding_dims, pretrained_w2v).get_model()
-model.load_weights(r"D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay1contentbest_weights.h5",by_name=True)
+model.load_weights(r"D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay1contentbest_weights.h5",by_name=True)
 model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 ##################################################################
 model.summary()
 print('Train...')
-fileweights = r"D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay1content_y2_best_weights.h5"
+fileweights = r"D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\weights\Ay1content_y2_best_weights.h5"
 checkpoint = ModelCheckpoint(fileweights, monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
 # checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
 early_stopping = EarlyStopping(monitor='val_acc', patience=5, mode='max')
@@ -68,7 +68,7 @@ predict = model.predict([emb_pre_label_x])
 predict = np.argmax(predict, axis=1)
 print(predict)
 print(np.shape(predict))
-pl.dump(predict, open('D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DBpedia_layer2_predict2_2', 'wb'))
+pl.dump(predict, open('D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DBpedia_layer2_predict2_2', 'wb'))
 
 pretrained_w2v, word_to_id, _ = pl.load(
     open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\PycharmProjects\Wos-Metadata2txt\data\wos\emb_matrix_glove_300', 'rb'))
@@ -84,7 +84,7 @@ y2 = ['actor', 'amusement park attraction', 'animal', 'artist', 'athlete', 'body
       'winter sport player', 'wrestler', 'writer', 'written work']
 # label1_id = [1,0,1,2,0,3,2,5,4,6,6]
 y2_id_pad = []
-label2_id =pl.load(open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DBpedia_layer2_predict2_2','rb'))
+label2_id =pl.load(open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DBpedia_layer2_predict2_2','rb'))
 
 for i in label2_id:
     y2_id_pad.append([word_to_id[x] for x in y2[i].split(' ') if x in word_to_id])
@@ -93,7 +93,7 @@ print(len(y2_id_pad))
 print(y2_id_pad[:10])
 y2_length = 3
 y2_pad = kr.preprocessing.sequence.pad_sequences(y2_id_pad, y2_length, padding='post', truncating='post')
-with open(r'D:\赵鲸朋\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DB_pre_y2_id_pad_2', 'wb') as f:
+with open(r'D:\E1106\pycharmModel0905\pycharmModel0905\htc-github\dbpedia\output\predictlabel\DB_pre_y2_id_pad_2', 'wb') as f:
     pl.dump(y2_pad, f)
 #######################################
 print("Time cost: %.3f seconds...\n" % (time.time() - start_time))
